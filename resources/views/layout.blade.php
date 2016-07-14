3f27b0836090ef730b53fc0846af958bafdd90ca
Bruno Monteiro <bruno.monteirodg@gmail.com>
2016-07-14 09:59:40 -0300

    layout principal

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Agenda</title>
+    </head>
+
+    <body>
+        <h3>Teste de view {{$teste}}</h3>
+    </body>
+</html>
+

e6f3046ef8811dced5cc2771b441f6610106a67a
br-monteiro <bruno.monteirodg@gmail.com>
2016-07-14 23:51:36 -0300

    Adicionado método que retorna todos os valores por letra do apelido

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -2,10 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Entities\Pessoa;
+
 class IndexController extends Controller
 {
     public function indexAction()
     {
         return view('layout');
+    }
+    
+    public function pessoasAction($letra = 'A')
+    {
+        // select * from TABELA where CAMPO like '%VALOR%'
+        $pessoas = Pessoa::where('apelido', 'like', $letra . '%')->get();
+        return response()->json($pessoas);
     }
 }
